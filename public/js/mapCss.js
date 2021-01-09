c507b91d429bd2f379736bd0ff14821d80ae55b8
Martin Lauff <lauff1234@gmail.com>
2021-01-09 18:53:22 +0100

    hopefully last save

--- public/js/mapCss.js
+++ public/js/mapCss.js
@@ -1,10 +1,9 @@
-$(document).ready(function () {
+$(document).ready(function() {
   // CSSMap;
   $('#map-continents').CSSMap({
     size: 1450,
     tooltips: 'floating-top-center',
     responsive: 'auto',
-    tapOnce: true,
-    mobileSupport: true,
+    tapOnce: true
   });
 });
